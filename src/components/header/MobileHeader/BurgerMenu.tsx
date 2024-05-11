092d429a438a0a6383144fc22a05faea1e5be004
sqVrt <sergeu_t@icloud.com>
2024-05-11 18:59:10 +0300

    refactor: :recycle: mobile header modernization

--- src/components/header/MobileHeader/BurgerMenu.tsx
+++ src/components/header/MobileHeader/BurgerMenu.tsx
@@ -1,20 +1,16 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
 import { useAppSelector } from "../../../app/hooks";
+
+import NavigationElement from "./NavigationElement";
 
 import {
   ProfileIcon,
-  SpeedometerIcon,
-  GlassesIcon,
+  ResumeIcon,
+  FolderIcon,
   PhoneIcon,
 } from "../../../assets/svg's";
 
 import classes from "./BurgerMenu.module.scss";
 import { COLORS } from "./../../../assets/colors";
-
-type IconMapType = {
-  [key: string]: string;
-};
 
 export default function BurgerMenu({
   isOpen,
@@ -25,25 +21,6 @@ export default function BurgerMenu({
 }) {
   const windowSize = useAppSelector((state) => state.windowSizeInfo.windowSize);
 
-  const location = useLocation();
-  const [activeIcon, setActiveIcon] = useState<string>("");
-
-  useEffect(() => {
-    const { pathname } = location;
-
-    const iconMap: IconMapType = {
-      "/": "profile",
-      "/skills": "skills",
-      "/experience": "experience",
-    };
-
-    setActiveIcon(iconMap[pathname]);
-  }, [location]);
-
-  const handleLinkClicked = () => {
-    setIsOpen(false);
-  };
-
   return (
     <div className={`${classes.container} ${isOpen && classes.open}`}>
       <div
@@ -51,42 +28,24 @@ export default function BurgerMenu({
         style={{ height: windowSize.height - 120 + "px" }}
       >
         <div className={classes.navigationContainer}>
-          <Link
-            to="/"
-            className={classes.iconContainer}
-            onClick={handleLinkClicked}
-          >
-            <ProfileIcon
-              className={`${classes.icon} ${
-                activeIcon === "profile" && classes.activeIcon
-              }`}
-            />
-            <p>Профиль</p>
-          </Link>
-          <Link
-            to="/skills"
-            className={classes.iconContainer}
-            onClick={handleLinkClicked}
-          >
-            <SpeedometerIcon
-              className={`${classes.icon} ${
-                activeIcon === "skills" && classes.activeIcon
-              }`}
-            />
-            <p>Навыки</p>
-          </Link>
-          <Link
-            to="/experience"
-            className={classes.iconContainer}
-            onClick={handleLinkClicked}
-          >
-            <GlassesIcon
-              className={`${classes.icon} ${
-                activeIcon === "experience" && classes.activeIcon
-              }`}
-            />
-            <p>Опыт работы</p>
-          </Link>
+          <NavigationElement
+            path="/"
+            icon={<ProfileIcon width={25} height={25} />}
+            name="About"
+            setIsOpen={setIsOpen}
+          />
+          <NavigationElement
+            path="/resume"
+            icon={<ResumeIcon width={25} height={25} />}
+            name="Resume"
+            setIsOpen={setIsOpen}
+          />
+          <NavigationElement
+            path="/projects"
+            icon={<FolderIcon width={25} height={25} />}
+            name="Projects"
+            setIsOpen={setIsOpen}
+          />
         </div>
         <div className={classes.contactMe}>
           <PhoneIcon fill={COLORS.aqua} width={40} height={40} />
